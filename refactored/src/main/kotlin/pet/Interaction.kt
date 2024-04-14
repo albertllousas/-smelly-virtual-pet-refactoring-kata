9bc085498994375e1f281979e1b4050c8dbdeb70
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-14 10:20:29 +0200

    Add scenario to Interaction: play chess

--- refactored/src/main/kotlin/pet/Interaction.kt
+++ refactored/src/main/kotlin/pet/Interaction.kt
@@ -7,5 +7,9 @@ fun interface Interaction {
         val playHideAndSeek = Interaction { pet ->
             VirtualPet(hunger = pet.hunger + 10, happiness = pet.happiness + 15, energy = pet.energy - 15)
         }
+
+        val playChess = Interaction { pet ->
+            VirtualPet(hunger = pet.hunger, happiness = pet.happiness + 10, energy = pet.energy - 5)
+        }
     }
 }
--- refactored/src/test/kotlin/pet/InteractionTest.kt
+++ refactored/src/test/kotlin/pet/InteractionTest.kt
@@ -2,6 +2,7 @@ package pet
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import pet.Interaction.Companion.playChess
 import pet.Interaction.Companion.playHideAndSeek
 
 class InteractionTest {
@@ -9,5 +10,10 @@ class InteractionTest {
     @Test
     fun `should play hide and seek`() {
         playHideAndSeek(VirtualPet()) shouldBe VirtualPet(hunger = 60, happiness = 65, energy = 35)
+    }
+
+    @Test
+    fun `should play chess`() {
+        playChess(VirtualPet()) shouldBe VirtualPet(hunger = 50, happiness = 60, energy = 45)
     }
 }
